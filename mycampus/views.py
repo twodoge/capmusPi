c60a3a48020ddfef5ae8110c81acbc7238f548bc
“ZhongQW <2384236461@qq.com>
2017-04-05 20:51:46 +0800

    修改一下找回密码功能

--- mycampus/views.py
+++ mycampus/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, render_to_response, HttpResponse, redirect,
 from django.http import HttpResponse
 from django.core.mail import EmailMultiAlternatives
 from . import models
+import random
 def login(request):
     return render(request, 'campus/login.html')
 
@@ -75,16 +76,53 @@ def forgotPassword(request):
 
 #发送邮件
 def setEmail(request):
-	email = request.POST.get('email', '')
-	user = models.User.objects.filter(email=email)
-	request.session['userName'] = user[0].userName
-	subject = '请重置您的密码'
-	text_content = '请重置您的密码'
-	html_content = '<p>这是一封<strong>重要的</strong>邮件.</p><a  href="http://192.168.1.104:8000/mycampus/rePassword/'+'">'+'点击这里重置密码</a>'
-	msg = EmailMultiAlternatives(subject, text_content, '2384236461@qq.com', [email])
-	msg.attach_alternative(html_content, "text/html")
-	msg.send()
-	return redirect('/mycampus/login/')
+	if request.is_ajax():
+		email = request.GET.get('email', '')
+		print(email+"1334485787")
+		user = models.User.objects.filter(email=email)
+		if user:
+			request.session['userName'] = user[0].userName
+			check = ""
+			i = 0
+			while i<4:
+				check+=str(random.randint(0,9))
+				i=i+1
+			request.session['check'] = check
+			subject = '请重置您的密码'
+			text_content = '请重置您的密码'
+			html_content = '您的验证码为:'+check
+			msg = EmailMultiAlternatives(subject, text_content, '2384236461@qq.com', [email])
+			msg.attach_alternative(html_content, "text/html")
+			msg.send()
+			error = ""
+			r = HttpResponse(error)
+			return r
+		else:
+			error = "该邮箱不存在，请前往注册"
+			r = HttpResponse(error)
+			return r
+
+def check(request):
+	if request.is_ajax():
+		email = request.GET.get('email', '')
+		checkcode = request.GET.get('checkcode', '')
+		print(email+checkcode)
+		user = models.User.objects.filter(email=email)
+		if user:
+			check = request.session.get('check')
+			if checkcode == check:
+				error = ""
+				r = HttpResponse(error)
+				return r
+			else:
+				error = "验证码错误"
+				r = HttpResponse(error)
+				return r
+		else:
+			error = "该邮箱不存在"
+			r = HttpResponse
+			return r
+
 # 重置密码页面	
 def rePassword(request):
 	return render(request, 'campus/rePassword.html')
